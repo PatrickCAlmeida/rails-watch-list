fbb867a05fbfdaeb92e170af284a36cefd877ad6
Patrick Almeida <patrick.c.almeida@gmail.com>
2022-03-19 17:40:31 +0100

    o

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,8 +1,5 @@
 // Load all the controllers within this directory and all subdirectories.
 // Controller files must be named *_controller.js.
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
 
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
